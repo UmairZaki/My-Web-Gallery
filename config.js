01060163c24f4de7e95bfa5a6efcbb251ce59bd2
Umair Zaki <umairzakicnbc@gmail.com>
2020-02-17 05:26:06 -0500

    My Web Gallery

---
+++ config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  siteTitle: 'Umair Zaki', // <title>
+  manifestName: 'Gallery',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-phantom/`, // This path is subpath of your hosting https://domain/portfolio
+  heading: 'Anubhav Srivastava',
+  subHeading: 'Full time Web Developer. Part time Open source contributor  ',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/anubhavsrivastava',
+    },
+    {
+      icon: 'fa-twitter',
+      name: 'Twitter',
+      url: 'https://twitter.com/onlyanubhav',
+    },
+    {
+      icon: 'fa-facebook',
+      name: 'Facebook',
+      url: 'https://facebook.com/theanubhav',
+    },
+  ],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,207 @@
+import React from 'react';
+
+import Layout from '../components/Layout';
+
+import pic1 from '../assets/images/1.jpg';
+import pic2 from '../assets/images/2.jpg';
+import pic3 from '../assets/images/3.jpg';
+import pic4 from '../assets/images/4.jpg';
+import pic5 from '../assets/images/5.jpg';
+import pic6 from '../assets/images/6.jpg';
+import pic7 from '../assets/images/7.jpg';
+import pic8 from '../assets/images/8.jpg';
+import pic9 from '../assets/images/9.jpg';
+import pic10 from '../assets/images/10.jpg';
+import pic11 from '../assets/images/11.jpg';
+import pic12 from '../assets/images/12.jpg';
+
+
+
+const IndexPage = () => (
+  <Layout>
+    <div id="main" className="b">
+      <div className="inner a ">
+        <header>
+          <h1>
+            My Web Gallery
+          </h1>
+        </header>
+        <section className="tiles">
+          <article className="style1">
+            <span className="image">
+              <img src={pic12} alt="" />
+            </span>
+            <a href="https://umairzaki-resume.netlify.com/" target="_blank">
+              <h2>Grayscale</h2>
+              <div className="content">
+                <p>
+                  A new stylish and unique grayscale theme for your new project.
+                </p>
+                <h3>Visit Site</h3>
+              </div>
+            </a>
+          </article>
+          <article className="style2">
+            <span className="image">
+              <img src={pic10} alt="" />
+            </span>
+            <a href="https://umairresume.netlify.com/" target="_blank">
+              <h2>Solid State</h2>
+              <div className="content">
+                <p>
+                A new stylish and unique solid state theme for your new project.
+                </p>
+                <h3>Visit Site</h3>
+              </div>
+            </a>
+          </article>
+          <article className="style3">
+            <span className="image">
+              <img src={pic9} alt="" />
+            </span>
+            <a href="https://umaircv.netlify.com/" target="_blank">
+              <h2>Spectral</h2>
+              <div className="content">
+                <p>
+                A new stylish and unique spectral theme for your new project.
+                </p>
+                <h3>Visit Site</h3>
+              </div>
+            </a>
+          </article>
+          <article className="style4">
+            <span className="image">
+              <img src={pic11} alt="" />
+            </span>
+            <a href="https://umairzaki-cv.netlify.com/" target="_blank">
+              <h2>Paradigmshift</h2>
+              <div className="content">
+                <p>
+                A new stylish and unique paradigmshift theme for your new project.
+                </p>
+                <h3>Visit Site</h3>
+              </div>
+            </a>
+          </article>
+          <article className="style5">
+            <span className="image">
+              <img src={pic1} alt="" />
+            </span>
+            <a href="https://umairnew.netlify.com/" target="_blank">
+              <h2>Highlights</h2>
+              <div className="content">
+                <p>
+                A new stylish and unique solid highlights theme for your new project.
+                </p>
+                <h3>Visit Site</h3>
+              </div>
+            </a>
+          </article>
+          <article className="style6">
+            <span className="image">
+              <img src={pic2} alt="" />
+            </span>
+            <a href="https://umair-zaki.netlify.com/" target="_blank">
+              <h2>Modern Resume</h2>
+              <div className="content">
+                <p>
+                A new stylish and unique modern resume theme for your new project.
+                </p>
+                <h3>Visit Site</h3>
+              </div>
+            </a>
+          </article>
+          <article className="style2">
+            <span className="image">
+              <img src={pic3} alt="" />
+            </span>
+            <a href="https://umairportfolio.netlify.com/" target="_blank">
+              <h2>Casual</h2>
+              <div className="content">
+                <p>
+                A new stylish and unique casual theme for your new project.
+                </p>
+                <h3>Visit Site</h3>
+              </div>
+            </a>
+          </article>
+          <article className="style4">
+            <span className="image">
+              <img src={pic4} alt="" />
+            </span>
+            <a href="https://umairzakicv.netlify.com/" target="_blank">
+              <h2>Dark Night</h2>
+              <div className="content">
+                <p>
+                A new stylish and unique dark night theme for your new project.
+                </p>
+                <h3>Visit Site</h3>
+              </div>
+            </a>
+          </article>
+          <article className="style6">
+            <span className="image">
+              <img src={pic5} alt="" />
+            </span>
+            <a href="https://umairzaki-new.netlify.com/" target="_blank">
+              <h2>Phantom</h2>
+              <div className="content">
+                <p>
+                A new stylish and unique phantom theme for your new project.
+                </p>
+                <h3>Visit Site</h3>
+              </div>
+            </a>
+          </article>
+          <article className="style5">
+            <span className="image">
+              <img src={pic6} alt="" />
+            </span>
+            <a href="https://umairzakiresume.netlify.com/" target="_blank">
+              <h2>Portfolio</h2>
+              <div className="content">
+                <p>
+                A new stylish and unique portfolio theme for your new project.
+                </p>
+                <h3>Visit Site</h3>
+              </div>
+            </a>
+          </article>
+          <article className="style3">
+            <span className="image">
+              <img src={pic7} alt="" />
+            </span>
+            <a href="https://umairzaki.netlify.com/" target="_blank">
+              <h2>Introduction</h2>
+              <div className="content">
+                <p>
+                A new stylish and unique introduction theme for your new project.
+                </p>
+                <h3>Visit Site</h3>
+              </div>
+            </a>
+          </article>
+          <article className="style1">
+            <span className="image">
+              <img src={pic8} alt="" />
+            </span>
+            <a href="https://umairzakiportfolio.netlify.com/" target="_blank">
+              <h2>Black</h2>
+              <div className="content">
+                <p>
+                A new stylish and unique solid black theme for your new project.
+                </p>
+                <h3>Visit Site</h3>
+              </div>
+            </a>
+          </article>
+        </section>
+        <ul className="copyright c">
+          <li>Umair Zaki | 03132873668</li>
+        </ul>
+      </div>
+    </div>
+  </Layout>
+);
+
+export default IndexPage;
